34f6ceb05a71347b5741c0a57f57b712196ded59
c.martynov <c.martynov@gmail.com>
2019-01-15 23:14:27 -0600

    Changed folder names

--- Rubiks cube solver/Properties/AssemblyInfo.cs
+++ Rubiks cube solver/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Raspberry win app")]
+[assembly: AssemblyTitle("Rubik's cube solver")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Raspberry win app")]
+[assembly: AssemblyProduct("Rubik's cube solver")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
